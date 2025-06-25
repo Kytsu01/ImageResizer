95a5c938ec817d296015e4e5c0e6136b5e93e602
Kytsu01 <nicolasmfrancisco@gmail.com>
2025-06-24 23:49:51 -0300

    feat: estrutura inicial do projeto

---
+++ ImageResizer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace ImageResizer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando redimensionador!");
+            Thread thread = new Thread(ResizeImage);
+            thread.Start();
+
+
+        }    
+
+        static void ResizeImage()
+        {
+
+            string inputDirectory = "Input_Files";
+            string resizedDirectory = "Resized_Files";
+            string finishedDirectory = "Finished_Files";
+
+            if(!Directory.Exists(inputDirectory))
+            {
+                Directory.CreateDirectory(inputDirectory);
+            }
+
+            if(!Directory.Exists(resizedDirectory))
+            {
+                Directory.CreateDirectory(resizedDirectory);
+            }
+
+            if(!Directory.Exists(finishedDirectory))
+            {
+                Directory.CreateDirectory(finishedDirectory);
+            }
+
+        }
+    }
+}
