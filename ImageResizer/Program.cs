a48da305ead5a9152ec079220c77f016fa15f9ae
Kytsu01 <nicolasmfrancisco@gmail.com>
2025-06-26 18:21:36 -0300

    feat: implementar protótipo funcional do redimensionamento de imagens

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.IO;
+using System.Drawing;
 
 namespace ImageResizer
 {
@@ -36,6 +37,53 @@ namespace ImageResizer
             {
                 Directory.CreateDirectory(finishedDirectory);
             }
+
+            FileStream fileStream;
+            FileInfo fileInfo;
+
+            int height = 200; 
+
+            while(true)
+            {
+                var inputFiles = Directory.EnumerateFiles(inputDirectory);
+
+                foreach(var file in inputFiles)
+                {
+                    fileStream = new FileStream(file, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+                    fileInfo = new FileInfo(file);
+
+                    string path = Environment.CurrentDirectory + @"\" + resizedDirectory + @"\" + DateTime.Now.Millisecond.ToString() + fileInfo.Name;
+
+                    Resize(Image.FromStream(fileStream), height, path);
+
+                    fileStream.Close();
+
+                    string finishedPath = Environment.CurrentDirectory + @"\" + finishedDirectory + @"\" + fileInfo.Name;
+
+                    fileInfo.MoveTo(finishedPath);
+
+                }
+
+                Thread.Sleep(new TimeSpan(0, 0, 3));
+            }
+
+        }
+
+        static void Resize(Image img, int height, string path)
+        {
+            double ratio = (double)height / img.Height;
+            int newWidth = (int)(img.Width * ratio);
+            int newHeight = (int)(img.Height * ratio);
+
+            Bitmap newImage = new Bitmap(newWidth, newHeight);
+
+            using (Graphics graphic = Graphics.FromImage(newImage))
+            {
+                graphic.DrawImage(img, 0, 0, newWidth, newHeight);
+            }
+
+            newImage.Save(path);
+            img.Dispose();
 
         }
     }
